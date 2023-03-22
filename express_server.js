ad5b59d81da56001f13bb1c59e06592ac4db07b7
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-21 18:17:40 -0600

    Setup the cookie. Implemented a log in button

--- express_server.js
+++ express_server.js
@@ -71,6 +71,12 @@ app.post('/urls/:id/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}!`);
 });
